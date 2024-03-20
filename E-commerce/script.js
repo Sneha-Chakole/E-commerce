1463bfd84bb443c7315da0f619ae6a2256549990
Sneha <snehachakole24@gmail.com>
2024-03-20 15:46:54 +0530

    E-commerce

---
+++ E-commerce/script.js
@@ -0,0 +1,15 @@
+const bars = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav =document.getElementById('navbar');
+
+if (bars) {
+    bars.addEventListener('click', () => {
+        nav.classList.add("active");
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove("active");
+    })
+}
